6268ae7547dc1d7fefb57a236f03c9a82155259e
Kuba Hofman <vertiqal123@centrum.cz>
2024-02-19 15:35:39 +0100

    final

--- WPP/functionality.js
+++ WPP/functionality.js
@@ -1,7 +1,8 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
-
+let on = false;
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     menu.classList.toggle('active');
+    
 })
